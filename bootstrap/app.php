c6402875c13601d09c0af835663722f117f56a0d
JuanDMeGon <contacto@juandmegon.com>
2015-10-02 00:15:31 -0500

    Configuring Lumen and Reviewing the Structure
    - Enabling the .env files.
    - Enabling Eloquent.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-// Dotenv::load(__DIR__.'/../');
+Dotenv::load(__DIR__.'/../');
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +21,7 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
